f1581c22f4fa72491432558019d66d8caeb7ccdd
tawakkalmamun <105841109521@student.unismuh.ac.id>
2024-05-29 15:09:40 +0800

    tawakkal

--- App.js
+++ App.js
@@ -1,25 +1,51 @@
 import { StyleSheet, Text, View, } from 'react-native';
+import React from 'react'
+const App = () => {
+  return(
+    <View style={{ flex: 1 }}>
+    <View style={{
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'end', // baris
+      justifyContent: 'end', // Kolom
 
-const App = () =>{
-  return (
-    <View style ={{ flex: 1 }}>
-    <View style={{ flex: 1, flexDirection:"row", backgroundColor:'white'}}>
-    <View style={{ flex: 1, backgroundColor:'yellow'}}>
-  </View>
-  <View style={{ flex: 1, backgroundColor:'green'}}>
-  </View>
-
-  </View>
-    <View style={{ flex: 1, flexDirection:'row', backgroundColor:'red'}}>
-    
-  
-  
-  <View style={{ flex: 1, backgroundColor:'brown'}}>
-  </View>
-  </View>
-  <View style={{ flex: 1, backgroundColor:'blue'}}>
-  </View>
-  </View>
+    }}>
+      <View style={{
+        flex:1,
+        backgroundColor: 'red', width: 130, height: 60,
+        justifyContent: 'center',
+        borderRadius: 10,
+        marginRight: 20,
+        marginLeft: 20,
+        marginBottom: 100,
+      }}>
+        <Text style={{
+          fontSize: 20 , color: 'white',
+          textAlign: 'center',
+          fontWeight: 'bold'
+        }}>
+          login
+        </Text>
+        </View>
+        <View style={{
+        flex:1,
+        backgroundColor: 'blue', width: 130, height: 60,
+        justifyContent: 'center',
+        borderRadius: 10,
+        marginRight: 20,
+        marginLeft: 20,
+        marginBottom: 100,
+      }}>  
+        <Text style={{
+          fontSize: 20 , color: 'white',
+          textAlign: 'center',
+          fontWeight: 'bold'
+        }}>
+          Sign up
+        </Text>
+        </View>
+      </View>
+      </View>
   )
 }
 export default App
