36a4e2719edb48315e319a8e8b0e152d936436e4
tawakkalmamun <105841109521@student.unismuh.ac.id>
2024-06-01 22:36:58 +0800

    push

--- App.js
+++ App.js
@@ -1,51 +1,64 @@
-import { StyleSheet, Text, View, } from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react'
-const App = () => {
+const App =  () => {
   return(
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1}}>
     <View style={{
-      flex: 1,
-      flexDirection: 'row',
-      alignItems: 'end', // baris
-      justifyContent: 'end', // Kolom
-
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+      marginTop:270
     }}>
-      <View style={{
-        flex:1,
-        backgroundColor: 'red', width: 130, height: 60,
-        justifyContent: 'center',
-        borderRadius: 10,
-        marginRight: 20,
-        marginLeft: 20,
-        marginBottom: 100,
+      <Image
+      source={require('./assets/webtoon.png')}
+      style={{
+        width:160,
+        height:150,
+        marginBottom: 50,
+      }}
+      />
+    </View>
+    <View style={{
+      flex:1,
+      flexDirection: 'row',
+      alignItems:'flex-end',
+      justifyContent:'center',
+      marginBottom: 100,
+    }}
+    >
+    <View style={{
+      flex:1,
+      backgroundColor: 'red',width: 130, height:60,
+      justifyContent: 'center',
+      borderRadius:10,
+      marginRight:10,
+      marginLeft:10
+    }}>
+      <Text style={{
+        fontSize: 20, color: 'white',
+        textAlign:'center',
+        fontWeight:'bold',
       }}>
-        <Text style={{
-          fontSize: 20 , color: 'white',
-          textAlign: 'center',
-          fontWeight: 'bold'
-        }}>
-          login
-        </Text>
-        </View>
-        <View style={{
-        flex:1,
-        backgroundColor: 'blue', width: 130, height: 60,
-        justifyContent: 'center',
-        borderRadius: 10,
-        marginRight: 20,
-        marginLeft: 20,
-        marginBottom: 100,
-      }}>  
-        <Text style={{
-          fontSize: 20 , color: 'white',
-          textAlign: 'center',
-          fontWeight: 'bold'
-        }}>
-          Sign up
-        </Text>
-        </View>
-      </View>
-      </View>
+        Login
+      </Text>
+    </View>
+    <View style={{
+      flex:1,
+      backgroundColor: 'blue',width: 130, height:60,
+      justifyContent: 'center',
+      borderRadius:10,
+      marginRight:10,
+    }}>
+      <Text style={{
+        fontSize: 20, color: 'white',
+        textAlign:'center',
+        fontWeight:'bold',
+      }}>
+        Sign up
+      </Text>
+    </View>
+    </View>
+    </View>
   )
 }
 export default App
