d2a4b341943172cc61eecfdedc4bc57a030fd5d3
tawakkalmamun <105841109521@student.unismuh.ac.id>
2024-06-14 15:15:59 +0800

    Textinput(105841109521)

--- App.js
+++ App.js
@@ -1,38 +1,35 @@
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, TextInput} from 'react-native';
 import React from 'react'
-import ButtonCustom from './component/Button/Button'
-const App = () => {
 
+const App = () => {
+  const TextInputCustom = ({ name, color}) => {
   return(
-    <View style={{ flex: 1}}>
-    <View style={{
-      flex:1,
-      justifyContent:'center',
-      alignItems:'center',
-      marginTop:270
-    }}>
-      <Image
-      source={require('./assets/webtoon.png')}
+      <TextInput
+      placeholder={`Masukkan ${name}`}
       style={{
-        width:160,
-        height:150,
-        marginBottom: 50,
+        height:40, 
+        width:200,
+        borderColor: 'Gray',
+        borderWidth: 1,
+        borderRadius: 10,
+        marginBottom:  10,
+        paddingLeft: 10,
+        color: color,
       }}
       />
-    </View>
-    <View style={{
-      flex:1,
-      flexDirection: 'row',
-      alignItems:'flex-end',
-      justifyContent:'center',
-      marginBottom: 100,
-    }}
-    >
-    <ButtonCustom color='blue' text = 'singin'/>
-    <ButtonCustom color='green' text = 'Sign up'/>
-    
-    </View>
-    </View>
+    )
+  }
+  return(
+      <View style= {{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+      }}>
+      <TextInputCustom name="Username" color="lavender"/>
+      <TextInputCustom name="Password" color="blue"/>
+      </View>
   )
 }
+
+ 
 export default App
